bddd27ef0582fa24816e1fdb85636dba20ff1169
LOLOROLL0 <56293082+LOLOROLL0@users.noreply.github.com>
2019-10-09 04:55:09 +0800

    Add files via upload

--- HW1-test3.py
+++ HW1-test3.py
@@ -1,7 +1,7 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if (c**2 == a**2 + b**2):
+if (c**2 == a**2 + b**2) or (a**2 == b**2 + c**2) or (b**2 == a**2 + c**2):
     print("YES")
 else:
-    print("NOPE")
+    print("NO")
