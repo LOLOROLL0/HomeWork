cdef7b065be7f0a571a6adaa341c3be3f82b793e
LOLOROLL0 <56293082+LOLOROLL0@users.noreply.github.com>
2019-10-15 01:14:42 +0300

    Add files via upload

---
+++ HW2-test4.py
@@ -0,0 +1,12 @@
+x1, y1=map(int, input().split())
+x2, y2=map(int, input().split())
+x3, y3=map(int, input().split())
+x4, y4=map(int, input().split())
+x5, y5=map(int, input().split())
+x6, y6=map(int, input().split())
+x7, y7=map(int, input().split())
+x8, y8=map(int, input().split())
+if x1+x2+x3+x4+x5+x6+x7+x8==y1+y2+y3+y4+y5+y6+y7+y8:
+    print("NO")
+else:
+    print("YES")
