d443bb4d591f407cdf274d09b31151f441f7e2eb
LOLOROLL0 <56293082+LOLOROLL0@users.noreply.github.com>
2019-10-15 00:34:55 +0300

    Add files via upload

---
+++ HW2-test2.py
@@ -0,0 +1,12 @@
+a=float(input())
+b=float(input())
+c=input()
+if c==("+"):
+    print(a+b)
+elif c==("-"):
+    print(a+b)
+elif c==("/") and a!=0 and b!=0:
+    print (a/b)
+else:
+    print('Nope')
+    
