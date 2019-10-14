763f6e5b44ceb14261413ac346aa1bbf92bae92d
LOLOROLL0 <56293082+LOLOROLL0@users.noreply.github.com>
2019-10-15 00:25:49 +0300

    Add files via upload

--- HW1-test5.py
+++ HW1-test5.py
@@ -6,14 +6,13 @@ b = float(input("b = "))
 c = float(input("c = "))
  
 discr = b ** 2 - 4 * a * c
-print("Дискриминант D = %.2f" % discr)
  
 if discr > 0:
     x1 = (-b + math.sqrt(discr)) / (2 * a)
     x2 = (-b - math.sqrt(discr)) / (2 * a)
-    print("X1: %.2f \nX2: %.2f" % (x1, x2))
+    print("X1:",x1,"X2",x2)
 elif discr == 0:
     x = -b / (2 * a)
-    print("x: %.2f" % x)
+    print("X1",x)
 else:
     print("Корней нет")
